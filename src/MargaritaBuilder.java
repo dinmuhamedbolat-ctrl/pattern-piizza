518d44caa951d840a47a93ec76720f7c2cd9614e
dinmuhamedbolat-ctrl <dinmuhamedbolat@gmail.com>
2026-09-13 15:43:39 +0500

    add pepperoni and margarita builder

---
+++ src/MargaritaBuilder.java
@@ -0,0 +1,39 @@
+// 3. ConcreteBuilder 1: Маргарита
+public class MargaritaBuilder implements PizzaBuilder {
+    private Pizza.Size size;
+    private Pizza.Dough dough;
+    private boolean hasCheese;
+    private boolean hasPepperoni;
+
+    @Override
+    public PizzaBuilder setSize(Pizza.Size size) {
+        this.size = size;
+        return this; // Возвращаем this для method chaining
+    }
+
+    @Override
+    public PizzaBuilder setDough(Pizza.Dough dough) {
+        this.dough = dough;
+        return this;
+    }
+
+    @Override
+    public PizzaBuilder addCheese() {
+        this.hasCheese = true;
+        return this;
+    }
+
+    @Override
+    public PizzaBuilder addPepperoni() {
+        this.hasPepperoni = false; // В Маргарите нет пепперони
+        return this;
+    }
+
+    @Override
+    public Pizza build() {
+        if (size == null || dough == null) {
+            throw new IllegalStateException("Size and Dough must be specified!");
+        }
+        return new Pizza(size, dough, hasCheese, hasPepperoni);
+    }
+}
