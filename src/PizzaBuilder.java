e96a479131799c5ed273facd31aa2464a200d547
dinmuhamedbolat-ctrl <dinmuhamedbolat@gmail.com>
2026-09-20 22:54:08 +0500

    add Waiter director and Client test class

--- src/PizzaBuilder.java
+++
@@ -1,8 +0,0 @@
-
-public interface PizzaBuilder {
-    PizzaBuilder setSize(Pizza.Size size);
-    PizzaBuilder setDough(Pizza.Dough dough);
-    PizzaBuilder addCheese();
-    PizzaBuilder addPepperoni();
-    Pizza build();
-}
--- src/Waiter.java
+++
@@ -1,9 +0,0 @@
-
-public class Waiter {
-    public Pizza constructClassicMargarita(PizzaBuilder builder) {
-        return builder.setSize(Pizza.Size.MEDIUM)
-                .setDough(Pizza.Dough.THIN)
-                .addCheese()
-                .build();
-    }
-}
